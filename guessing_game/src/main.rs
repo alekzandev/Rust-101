3218345c57fc27f81efd9fba2eaf39bd22eb4b1d
Johny Vallejo-Sánchez <35435216+Hausdorff94@users.noreply.github.com>
2023-05-22 04:46:53 +0000

    Add secret number with Crate rand

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -3,16 +3,22 @@
 // prelude is a set of items defined in std
 
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100); // inclusive, exclusive
+
+    println!("The secret number is: {secret_number}");
+
     println!("Please input your guess.");
 
-    let mut guess = String::new();
+    let mut guess = String::new(); // mutable variable
 
     io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        .read_line(&mut guess) // & indicates reference
+        .expect("Failed to read line"); // expect is a method of io::Result
 
     println!("You guessed: {guess}");
 }
