94e57828d85ffc3a9b7a391d12d36ed2e3a6f4b5
Johny Vallejo-Sánchez <35435216+Hausdorff94@users.noreply.github.com>
2023-05-22 04:13:58 +0000

    Update guessing game script

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,9 +1,13 @@
+// bring io library into scope
+// std is the standard library
+// prelude is a set of items defined in std
+
 use std::io;
 
 fn main() {
     println!("Guess the number!");
     println!("Please input your guess.");
-    
+
     let mut guess = String::new();
 
     io::stdin()
