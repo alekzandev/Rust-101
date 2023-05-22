2610f1363ed43e64d73413fe66969dba766bb5a0
Johny Vallejo-Sánchez <35435216+Hausdorff94@users.noreply.github.com>
2023-05-22 03:53:23 +0000

    Retake course

--- guessing_game/target/debug/deps/guessing_game-bafb5e096068fa4a.d
+++
@@ -1,5 +0,0 @@
-/workspaces/Rust-101/guessing_game/target/debug/deps/guessing_game-bafb5e096068fa4a: src/main.rs
-
-/workspaces/Rust-101/guessing_game/target/debug/deps/guessing_game-bafb5e096068fa4a.d: src/main.rs
-
-src/main.rs:
--- guessing_game/target/debug/guessing_game.d
+++
@@ -1,1 +0,0 @@
-/workspaces/Rust-101/guessing_game/target/debug/guessing_game: /workspaces/Rust-101/guessing_game/src/main.rs
