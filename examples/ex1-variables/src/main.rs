e179ed30aff28095fbf11e882881beb0f5d75b78
Hausdorff94 <jhonyal58@gmail.com>
2024-02-25 22:44:43 -0500

    Add mutable examples

--- examples/ex1-variables/src/main.rs
+++ examples/ex1-variables/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    let message = "Name: Hausdorff, Weight: ";
+    let weight = 68.0;
+
+    let pounds = 2.2046 * weight as f64;
+    println!("{}{} kilos", message, weight);
+    println!("{}{} pounds", message, pounds);
 }
