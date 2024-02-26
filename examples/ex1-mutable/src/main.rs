00cbece399edbbc0d8419519ffeab0d46abd1e5c
Hausdorff94 <jhonyal58@gmail.com>
2024-02-25 22:43:34 -0500

    add mutable example

---
+++ examples/ex1-mutable/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let mut message = String::from("Name: Hausdorff, Height: ");
+    message.clear();
+
+    let mut height = 1.74;
+    height = 1.75;
+
+    println!("{}{} meters", message, height);
+}
