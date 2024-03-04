b4053f83bf8a3c50495f610c273582137f75d375
Hausdorff94 <jhonyal58@gmail.com>
2024-03-03 23:58:25 -0500

    Add loops control

---
+++ examples/ex4-loops/src/main.rs
@@ -0,0 +1,21 @@
+// The loop and if let expressions
+
+fn main() {
+    let mut x = 1;
+    loop {
+        println!("x is {}", x);
+        x += 1;
+        if x > 5 {
+            break;
+        }
+    }
+
+    //let maybe_number: Option<Option<()>> = Some(None);
+    let maybe_number = Some(77);
+    if let Some(number) = maybe_number {
+        println!("The number is {:#?}", number);
+    } else {
+        println!("There is no number");
+    }
+}
+
