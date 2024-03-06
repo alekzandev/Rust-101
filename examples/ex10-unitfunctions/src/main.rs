5f62cc75edf59f22885433b6a085e0ed861dea10
Hausdorff94 <jhonyal58@gmail.com>
2024-03-05 23:50:47 -0500

    Add return values

--- examples/ex10-unitfunctions/src/main.rs
+++ examples/ex10-unitfunctions/src/main.rs
@@ -1,21 +1,57 @@
-fn process_numbers (numbers: &[i32]) {
+use std::io;
+
+fn process_numbers(numbers: &[i32], option: &str) {
     let mut sum = 0;
 
     for number in numbers {
         sum += number;
     }
 
-    println!("The sum is: {}", sum);
+    if option == "1" {
+        println!("The sum is: {}", sum);
 
-    if sum % 2 == 0 {
-        println!("The sum is even")
+        if sum % 2 == 0 {
+            println!("The sum is even")
+        } else {
+            println!("The sum is odd")
+        }
+    } else if option == "2"{
+        let avg = sum as f64 / numbers.len() as f64;
+        println!("The average is: {}", avg);
     } else {
-        println!("The sum is odd")
+        // Stop the program
+        println!("Invalid option");
+        
     }
+}
 
+fn parse_str_to_i32 (input: &str) -> i32 {
+    input.trim().parse().unwrap()
 }
 
 fn main(){
-    let numbers = [1, 2, 3, 4, 5];
-    process_numbers(&numbers);
+    let mut option = String::new();
+    println!("Enter the option: 1.Sum \t 2.Average");
+    io::stdin().read_line(&mut option).unwrap();
+    let option = option.trim();
+
+    let mut input = String::new();
+    println!("Enter the number of elements: ");
+    io::stdin().read_line(&mut input).unwrap();
+    let n = parse_str_to_i32(&input);
+
+    let mut numbers = Vec::<i32>::new();
+
+    for i in 1..=n {
+        println!("Enter the number {}: ", i);
+        input.clear();
+        io::stdin().read_line(&mut input).unwrap();
+        let number = parse_str_to_i32(&input);
+        numbers.push(number);
+    }
+    match option.trim(){
+        "1" | "2"=> process_numbers(&numbers, option),
+        _ => println!("Invalid option for processing numbers. Please enter 1 or 2.")
+    }
+
 }
