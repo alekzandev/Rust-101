7aa925213d89a064a60f2357c3ab323e2f6ee32b
Johny Vallejo-Sánchez <35435216+Hausdorff94@users.noreply.github.com>
2024-03-22 22:31:54 +0000

    Lab week3

--- examples/ex21-define-enum/src/main.rs
+++ examples/ex21-define-enum/src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 enum DiskType {
     SSD,
     HDD,
@@ -5,24 +6,35 @@ enum DiskType {
 }
 
 #[derive(Debug)]
-enum DiskSize {
-    KB(u32),
-    MB(u32),
-    GB(u32),
-    TB(u32),
+enum FileSize {
+    B(usize),
+    KB(usize),
+    MB(usize),
+    GB(usize),
+    TB(usize),
+}
+
+fn format_size(size: usize) -> String {
+    let filesize = match size {
+        0..=999 => FileSize::B(size),
+        1000..=999_999 => FileSize::KB(size / 1000),
+        1_000_000..=999_999_999 => FileSize::MB(size / 1_000_000),
+        1_000_000_000..=999_999_999_999 => FileSize::GB(size / 1_000_000_000),
+        _ => FileSize::TB(size / 1_000_000_000_000)
+    };
+    
+    match filesize {
+        FileSize::B(bytes) => format!("{:?}", bytes),
+        FileSize::KB(kb) => format!("{:?} KB", kb),
+        FileSize::MB(mb) => format!("{:?} MB", mb),
+        FileSize::GB(gb) => format!("{:?} GB", gb),
+        FileSize::TB(tb) => format!("{:?} TB", tb),
+    }
 }
 
 fn main() {
-    let disk_type = DiskType::SSD;
-    match disk_type {
-        DiskType::SSD => println!("Solid State Drive"),
-        DiskType::HDD => println!("Hard Disk Drive"),
-        DiskType::Hybrid => println!("Hybrid Drive"),
-    }
-
-    let disk_size = DiskSize::GB(512);
-    println!("{:?}", disk_size);
-
-    let disk_size = DiskSize::TB(1);
-    println!("{:?}", disk_size);
+    let disk = DiskType::SSD;
+    let size = 12340032432324;
+    println!("Disk type: {:?}", disk);
+    println!("Size: {}", format_size(size));
 }
