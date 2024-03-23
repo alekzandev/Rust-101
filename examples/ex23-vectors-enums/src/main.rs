b5f057354deb86867c50558963e8e0ebf3f0ef3a
Johny Vallejo-Sánchez <35435216+Hausdorff94@users.noreply.github.com>
2024-03-23 00:10:40 +0000

    Add vectors with enum

--- examples/ex23-vectors-enums/src/main.rs
+++ examples/ex23-vectors-enums/src/main.rs
@@ -1,7 +1,21 @@
+#[derive(Debug)]
 enum Shape {
     Circle(f64),
     Rectangle(f64, f64),
     Square(f64),
+    Triangle(f64, f64, f64),
+}
+
+fn calculate_are(shape: &Shape) -> f64 {
+    match shape {
+        Shape::Circle(radius) => std::f64::consts::PI * radius * radius,
+        Shape::Rectangle(width, height) => width * height,
+        Shape::Square(side) => side * side,
+        Shape::Triangle(a, b, c) => {
+            let s = (a + b + c) / 2.0;
+            (s * (s - a) * (s - b) * (s - c)).sqrt()
+        }
+    }
 }
 
 fn main() {
@@ -9,14 +23,16 @@ fn main() {
         Shape::Circle(3.0),
         Shape::Rectangle(2.0, 3.0),
         Shape::Square(4.0),
+        Shape::Triangle(3.0, 4.0, 5.0),
     ];
-    let total_area: f64 = shapes
-        .iter()
-        .map(|shape| match shape {
-            Shape::Circle(radius) => std::f64::consts::PI * radius * radius,
-            Shape::Rectangle(width, height) => width * height,
-            Shape::Square(side) => side * side,
+    let mut largest_area: f64 = 0.0;
+    let mut largest_shape = &Shape::Circle(0.0);
+    for shape in &shapes {
+        let area = calculate_are(&shape);
+        if area > largest_area {
+            largest_area = area;
+            largest_shape = shape;
         }
-    ).sum();
-    println!("Total area: {}", total_area);
+    }
+    println!("The largest area shape is the {:?} with: {} sq units.", largest_shape, largest_area);
 }
